4165f6bf298a8524ff4e74b99992c26848c3ba50
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-19 18:57:42 +0100

    adding nodemon

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.get('/about/:name', (req, res) => {
-    let user = req.params.name;
-    res.send('<h1>About ' + user + '</h1>');
-});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000...');
