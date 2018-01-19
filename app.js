36aa7aa306cc4cd8a7cdd83a20a5feebf5091ed2
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-19 18:39:31 +0100

    routes and params

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/about/:name', (req, res) => {
+    let user = req.params.name;
+    res.send('<h1>About ' + user + '</h1>');
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000...');
 });
