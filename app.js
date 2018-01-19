39e2ab32a082a4b40b880895a83b62505119ec38
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-19 20:18:20 +0100

    adding a form

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
-
+const bodyParser = require('body-parser');
 const app = express();
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
 // Set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,6 +35,21 @@ app.get('/users', (req, res) => {
     },
   ];
   res.json(users);
+});
+
+app.get('/download', (req, res) => {
+  res.download(path.join(__dirname, '/download/pdf.pdf'));
+});
+
+app.get('/about', (req, res) => {
+  res.redirect('/about.html');
+});
+
+app.post('/subscribe', (req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+
+  console.log(name + ' has subscribed with ' + email);
 });
 
 
